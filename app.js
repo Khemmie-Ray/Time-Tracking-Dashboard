4395b733811f9d3bbfd3706ba530b9816ddd252f
Khemmie-Ray <atokemmy@gmail.com>
2023-06-25 16:23:59 +0100

    add desktop view of app

---
+++ app.js
@@ -0,0 +1,67 @@
+import data from "./data.js"
+
+const container = document.querySelector('.card-container')
+const dailyBtn = document.getElementById("daily")
+const weeklyBtn = document.getElementById("weekly")
+const monthlyBtn = document.getElementById("monthly")
+
+container.innerHTML = data.map(dailyUpdate).join('');
+dailyBtn.addEventListener('click', () => {
+    container.innerHTML = data.map(dailyUpdate).join('');
+});
+
+weeklyBtn.addEventListener('click', () => {
+    container.innerHTML = data.map(weeklyUpdate).join('');
+});
+
+monthlyBtn.addEventListener('click', () => {
+    container.innerHTML = data.map(monthlyUpdate).join('');
+});
+
+function dailyUpdate(info) {
+    const cardElement = `
+    <div class="card">
+          <div class="topBg color-${info.id}">
+            <img class="icons" src=${info.image} alt="">
+          </div>
+          <div class="content">
+            <h3 class="card-title">${info.title} <span><img class="dot" src="images/icon-ellipsis.svg" alt=""></span></h3>
+            <h2>${info.timeframes.daily.current}hrs</h2>
+            <p>Last Week - ${info.timeframes.daily.previous}hrs</p>
+          </div>
+    </div>`
+
+return cardElement;
+}
+
+function weeklyUpdate(info) {
+    const cardElement = `
+    <div class="card">
+          <div class="topBg color-${info.id}">
+            <img class="icons" src=${info.image} alt="">
+          </div>
+          <div class="content">
+            <h3 class="card-title">${info.title} <span><img class="dot" src="images/icon-ellipsis.svg" alt=""></span></h3>
+            <h2>${info.timeframes.weekly.current}hrs</h2>
+            <p>Last Week - ${info.timeframes.weekly.previous}hrs</p>
+          </div>
+    </div>`
+
+return cardElement;
+}
+
+function monthlyUpdate(info) {
+    const cardElement = `
+    <div class="card">
+          <div class="topBg color-${info.id}">
+            <img class="icons" src=${info.image} alt="">
+          </div>
+          <div class="content">
+            <h3 class="card-title">${info.title} <span><img class="dot" src="images/icon-ellipsis.svg" alt=""></span></h3>
+            <h2>${info.timeframes.monthly.current}hrs</h2>
+            <p>Last Week - ${info.timeframes.monthly.previous}hrs</p>
+          </div>
+    </div>`
+
+return cardElement;
+}
